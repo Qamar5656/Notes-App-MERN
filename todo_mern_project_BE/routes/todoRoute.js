fbf9a1d2e5caca8d6d5fd2c82e1aba4546d4b96a
Qamar5656 <qamar4157@gmail.com>
2025-12-02 16:07:39 +0500

    updating todo routes according to the todo

--- todo_mern_project_BE/routes/todoRoute.js
+++ todo_mern_project_BE/routes/todoRoute.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { createTodo, getTodos } from "../controllers/todoController.js";
+import {
+  createTodo,
+  DeleteTodo,
+  getTodos,
+  updateTodos,
+} from "../controllers/todoController.js";
 
 const router = express.Router();
 
 //routes
 router.post("/create-todo", createTodo);
 router.get("/todos", getTodos);
+router.put("/todos/:id", updateTodos);
+router.delete("/todos/:id", DeleteTodo);
 
 export default router;
