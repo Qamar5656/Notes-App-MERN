996f4c445e73f67d8f6d45226a89b706c3a050d8
Qamar5656 <qamar4157@gmail.com>
2025-12-02 16:07:23 +0500

    adding update and delete api in controller file

--- todo_mern_project_BE/controllers/todoController.js
+++ todo_mern_project_BE/controllers/todoController.js
@@ -54,4 +54,59 @@ export const getTodos = async (req, res) => {
   }
 };
 
-export const updateTodos = async (req, res) => {};
+//Update todo
+export const updateTodos = async (req, res) => {
+  const { id } = req.params;
+  const { task, description } = req.body;
+
+  console.log("id value is ", id);
+  try {
+    //find todo from database
+    const todo = await Todo.findById(id);
+
+    //if no todo found
+    if (!todo) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Task Not Found" });
+    }
+
+    //update provided fields
+    todo.task = task || todo.task;
+    todo.description = description || todo.description;
+
+    //Save Updated todo
+    const updatedTodo = await todo.save();
+
+    //correct response
+    res.status(200).json({
+      success: true,
+      message: "Todo updated successfully",
+      todo: updatedTodo,
+    });
+  } catch (error) {
+    console.error("Backend Error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error. Please try again later.",
+    });
+  }
+};
+
+//Delete todo
+export const DeleteTodo = async (req, res) => {
+  const { id } = req.params; // get user ID from route
+
+  try {
+    const todo = await Todo.findOneAndDelete({ _id: id });
+
+    if (!todo) {
+      return res.status(404).json({ message: "todo not found" });
+    }
+
+    res.status(200).json({ message: "todo deleted successfully", todo });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server Error" });
+  }
+};
