858aff10b3c2fae9d53497e44abaa69d790d8432
Qamar5656 <qamar4157@gmail.com>
2025-12-02 18:11:53 +0500

    adding toast message container  in main fille

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -8,70 +8,86 @@ import ProtectedRoute from "./routes/ProtectedRoute.jsx";
 import VerifyOtp from "./components/Users/OtpForm/VerifyOtp.jsx";
 import ForgetPassword from "./components/Users/forgetPassword/ForgetPassword.jsx";
 import ResetPassword from "./components/Users/resetPassword/ResetPassword.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public Routes */}
-        <Route
-          path="/signin"
-          element={
-            <PublicRoute>
-              <SignIn />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/signup"
-          element={
-            <PublicRoute>
-              <SignUp />
-            </PublicRoute>
-          }
-        />
+    <>
+      {/* Toast Container  */}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      {/* Public Routes */}
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/signin"
+            element={
+              <PublicRoute>
+                <SignIn />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <PublicRoute>
+                <SignUp />
+              </PublicRoute>
+            }
+          />
 
-        <Route
-          path="/verify-otp"
-          element={
-            <PublicRoute>
-              <VerifyOtp />
-            </PublicRoute>
-          }
-        />
+          <Route
+            path="/verify-otp"
+            element={
+              <PublicRoute>
+                <VerifyOtp />
+              </PublicRoute>
+            }
+          />
 
-        <Route
-          path="/forget-password"
-          element={
-            <PublicRoute>
-              <ForgetPassword />
-            </PublicRoute>
-          }
-        />
+          <Route
+            path="/forget-password"
+            element={
+              <PublicRoute>
+                <ForgetPassword />
+              </PublicRoute>
+            }
+          />
 
-        <Route
-          path="/reset-password"
-          element={
-            <PublicRoute>
-              <ResetPassword />
-            </PublicRoute>
-          }
-        />
+          <Route
+            path="/reset-password"
+            element={
+              <PublicRoute>
+                <ResetPassword />
+              </PublicRoute>
+            }
+          />
 
-        {/* Protected Routes */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+          {/* Protected Routes */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Redirect any unknown route to SignIn */}
-        <Route path="*" element={<Navigate to="/signin" replace />} />
-      </Routes>
-    </BrowserRouter>
+          {/* Redirect any unknown route to SignIn */}
+          <Route path="*" element={<Navigate to="/signin" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
