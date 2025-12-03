19bd28297b5f2b5e68812ab4958683b194e7e5a9
Qamar5656 <qamar4157@gmail.com>
2025-12-03 18:03:31 +0500

    adding search icon in navbar component

--- todo/src/components/Dashboard/Navbar/Navbar.jsx
+++ todo/src/components/Dashboard/Navbar/Navbar.jsx
@@ -3,9 +3,19 @@ import { FiCheckSquare } from "react-icons/fi";
 import { FaSearch } from "react-icons/fa";
 import Button from "../../Button/Button";
 import AddTodo from "../../Todos/AddTodo";
+import SearchTodo from "../../Todos/SearchTodo";
 
 const Navbar = ({ selectedItem, onTaskAdded }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [searchmodal, setSearchModal] = useState(false);
+
+  const openSearch = () => {
+    setSearchModal(true);
+  };
+
+  const closeSearch = () => {
+    setSearchModal(false);
+  };
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -29,7 +39,11 @@ const Navbar = ({ selectedItem, onTaskAdded }) => {
 
           {/* Right side: Search + Add Task */}
           <div className="flex items-center gap-3">
-            <FaSearch size={24} className="text-gray-600 cursor-pointer" />
+            <FaSearch
+              size={24}
+              className="text-gray-600 cursor-pointer"
+              onClick={openSearch}
+            />
             <Button
               caption="+ Add Task"
               className="bg-gray-800 hover:bg-gray-900"
@@ -39,19 +53,37 @@ const Navbar = ({ selectedItem, onTaskAdded }) => {
         </div>
       </nav>
 
+      {/* Search functionality  */}
+      {searchmodal && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+          <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6 relative">
+            {/* Close button */}
+            <button
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-800 text-5xl font-bold"
+              onClick={closeSearch}
+            >
+              &times;
+            </button>
+
+            {/* Search Input  */}
+            <SearchTodo />
+          </div>
+        </div>
+      )}
+
       {/* Modal Overlay */}
       {isModalOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
           <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6 relative">
             {/* Close button */}
             <button
-              className="absolute top-2 right-2 text-gray-500 hover:text-gray-800 text-xl font-bold"
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-800 text-5xl font-bold"
               onClick={closeModal}
             >
               &times;
             </button>
 
-            {/* AddTodo Form */}
+            {/* Todo form */}
             <AddTodo onTodoAdded={handleTaskAdded} />
           </div>
         </div>
