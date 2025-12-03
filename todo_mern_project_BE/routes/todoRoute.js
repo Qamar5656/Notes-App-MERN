a39daf4e3e025cdf4ae353932f9eaaa6b7f5759a
Qamar5656 <qamar4157@gmail.com>
2025-12-03 18:15:20 +0500

    updating todo routes file

--- todo_mern_project_BE/routes/todoRoute.js
+++ todo_mern_project_BE/routes/todoRoute.js
@@ -1,17 +1,26 @@
 import express from "express";
 import {
   createTodo,
-  DeleteTodo,
   getTodos,
   updateTodos,
+  DeleteTodo,
+  permanentDeleteTodo,
+  markAsCompleted,
+  restoreTodo,
+  getTodoStats,
 } from "../controllers/todoController.js";
+import { authenticateUser } from "../middleware/AuthMiddleware.js";
 
 const router = express.Router();
 
-//routes
-router.post("/create-todo", createTodo);
-router.get("/todos", getTodos);
-router.put("/todos/:id", updateTodos);
-router.delete("/todos/:id", DeleteTodo);
+// All routes protected
+router.post("/create-todo", authenticateUser, createTodo);
+router.get("/todos", authenticateUser, getTodos); //
+router.put("todos/:id", authenticateUser, updateTodos);
+router.delete("/todos/:id", authenticateUser, DeleteTodo);
+router.delete("/todos/:id/permanent", authenticateUser, permanentDeleteTodo);
+router.patch("/todos/:id/complete", authenticateUser, markAsCompleted);
+router.patch("/todos/:id/restore", authenticateUser, restoreTodo);
+router.get("/todos/stats", authenticateUser, getTodoStats);
 
 export default router;
