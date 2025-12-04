61813b970807a091698399d03f2a590bdac588dd
Qamar5656 <qamar4157@gmail.com>
2025-12-04 18:36:51 +0500

    updating main file of frontend

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -9,106 +9,118 @@ import VerifyOtp from "./components/Users/OtpForm/VerifyOtp.jsx";
 import ForgetPassword from "./components/Users/forgetPassword/ForgetPassword.jsx";
 import ResetPassword from "./components/Users/resetPassword/ResetPassword.jsx";
 import { ToastContainer } from "react-toastify";
+import { ModalProvider } from "./context/ModalContext";
+import ConfirmModal from "./components/Common/ConfirmModel.jsx";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
-      {/* Toast Container  */}
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      <BrowserRouter>
-        <Routes>
-          {/* Public Routes */}
-          <Route
-            path="/signin"
-            element={
-              <PublicRoute>
-                <SignIn />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              <PublicRoute>
-                <SignUp />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/verify-otp"
-            element={
-              <PublicRoute>
-                <VerifyOtp />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/forget-password"
-            element={
-              <PublicRoute>
-                <ForgetPassword />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/reset-password"
-            element={
-              <PublicRoute>
-                <ResetPassword />
-              </PublicRoute>
-            }
-          />
-          {/* Protected Dashboard Routes */}
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
+      <ModalProvider>
+        <BrowserRouter>
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
           />
 
-          <Route
-            path="/dashboard/priority/:name"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
+          <ConfirmModal />
 
-          <Route
-            path="/dashboard/notebook/:name"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
+          <Routes>
+            {/* Public Routes */}
+            <Route
+              path="/signin"
+              element={
+                <PublicRoute>
+                  <SignIn />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <PublicRoute>
+                  <SignUp />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/verify-otp"
+              element={
+                <PublicRoute>
+                  <VerifyOtp />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/forget-password"
+              element={
+                <PublicRoute>
+                  <ForgetPassword />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/reset-password"
+              element={
+                <PublicRoute>
+                  <ResetPassword />
+                </PublicRoute>
+              }
+            />
 
-          <Route
-            path="/dashboard/:section"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          {/* Redirect any unknown route to SignIn */}
-          <Route path="*" element={<Navigate to="/signin" replace />} />
-        </Routes>
-      </BrowserRouter>
+            {/* Protected Dashboard Routes - CORRECT ORDER! */}
+
+            {/* 1. Most specific: Two-path routes */}
+            <Route
+              path="/dashboard/priority/:name"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/dashboard/notebook/:name"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* 2. Less specific: One-path routes */}
+            <Route
+              path="/dashboard/:section"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* 3. Least specific: Base route */}
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* Redirect any unknown route to SignIn */}
+            <Route path="*" element={<Navigate to="/signin" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </ModalProvider>
     </>
   );
 };
