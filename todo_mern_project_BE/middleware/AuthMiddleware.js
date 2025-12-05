27fa114256f2dae95bbb09d7c1e723dc742c5a06
Qamar5656 <qamar4157@gmail.com>
2025-12-05 15:56:12 +0500

    auth middleware for user authentication

--- todo_mern_project_BE/middleware/AuthMiddleware.js
+++ todo_mern_project_BE/middleware/AuthMiddleware.js
@@ -1,16 +1,15 @@
 import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET || "your_secret_key";
-
 export const authenticateUser = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; //Bearer Token
-
-  //If no token display the error
-  if (!token) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
     return res
       .status(401)
-      .json({ success: false, message: "Unautorized User" });
+      .json({ success: false, message: "Unauthorized User" });
   }
+
+  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
