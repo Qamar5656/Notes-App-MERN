29cab614ddb3c205d78a21bffdbdd19001c9f2a2
Qamar5656 <qamar4157@gmail.com>
2025-12-08 15:22:49 +0500

    updating the table component

--- todo/src/components/Dashboard/Table/Table.jsx
+++ todo/src/components/Dashboard/Table/Table.jsx
@@ -271,7 +271,8 @@ const Table = ({
 
           {/* Pagination */}
 
-          {paginatedTodos.length > 0 ? (
+          {/* {paginatedTodos.length > 9 ? ( */}
+          {computedTotalPages > 1 ? (
             <div className="flex justify-center items-center gap-4 mt-4">
               <button
                 className="px-4 py-2 bg-gray-200 rounded disabled:opacity-50 hover:bg-gray-300"
